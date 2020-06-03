ca3674b5b91de1dcb0eb8b3a56945406c51b0b93
mayuki-k <ymtech0615@gmail.com>
2020-06-03 22:30:17 +0900

    CloudStorageへUpload処理追加

--- cloudstorage.py
+++ cloudstorage.py
@@ -4,6 +4,17 @@ from google.oauth2.service_account import Credentials
 credentials = Credentials.from_service_account_file('env/auth.json')
 client = storage.Client(credentials=credentials, project=credentials.project_id)
 
-bucket = client.get_bucket('mayuki-test-bucket')
-blob = bucket.blob('data.txt')
-blob.upload_from_string('HelloWorld')
+
+
+def upload_from_string():
+    bucket = client.get_bucket('mayuki-test-bucket')
+    blob = bucket.blob('data.txt')
+    blob.upload_from_string('HelloWorld')
+
+def upload_from_file():
+    with open('data.csv', 'rb') as f:
+        bucket = client.get_bucket('mayuki-test-bucket')
+        blob = bucket.blob('test.txt')
+        blob.upload_from_file(f)
+
+upload_from_file()
