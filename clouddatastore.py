cdc4181398885b49b0482983be80e4375f3b06a6
mayuki-k <ymtech0615@gmail.com>
2020-06-08 23:35:25 +0900

    CloudDataStore処理追加

---
+++ clouddatastore.py
@@ -0,0 +1,17 @@
+from google.cloud import datastore
+from google.oauth2.service_account import Credentials
+
+from env import const
+
+credentials = Credentials.from_service_account_file(const.PATH)
+client = datastore.Client(credentials=credentials, project=credentials.project_id)
+
+def save(kind, id, datas):
+    key = client.key(kind, id)
+    put_datas = datastore.Entity(key=key)
+    for key in datas.keys():
+        put_datas[key] = datas[key]
+    client.put(put_datas)
+
+    
+
